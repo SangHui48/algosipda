25ba336e1bbaa47d592e11ea4264a9b2fe397176
한상희 <70365377+SangHui48@users.noreply.github.com>
2023-10-10 14:59:59 +0900

    list comprehension은 어떻게 하는걸까?

--- baekjoon/2576_홀수.py
+++ baekjoon/2576_홀수.py
@@ -2,13 +2,12 @@ import sys
 
 input = sys.stdin.readline
 
-# arr = []
-# for _ in range(7):
-#     data = int(input())
-#     if data % 2 == 1:
-#         arr.append(data)
-arr = [int(input()) for _ in range(7) if int(input()) % 2 != 0]
-print(arr)
+arr = []
+for _ in range(7):
+    data = int(input())
+    if data % 2 == 1:
+        arr.append(data)
+# arr = [int(input().strip()) for _ in range(7) if int(input().strip()) % 2 == 1]
 if len(arr):
     arr.sort()
     print(sum(arr))
