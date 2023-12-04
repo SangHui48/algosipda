c603448f1f6c3313fd385cef832e9d91a00e797d
한상희 <70365377+SangHui48@users.noreply.github.com>
2023-12-04 14:11:22 +0900

    tmp

---
+++ baekjoon/1406_에디터.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+text = input()
+M = int(input())
+i = len(text)
+for _ in range(M):
+    commands = list(input().split())
+    
+    if commands[0] == 'L':
+        pass
+    elif commands[0] == 'D':
+        pass
+    elif commands[0] == 'B':
+        pass
+    elif commands[0] == 'P':
+        pass
