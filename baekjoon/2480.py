86312f4dda309293a61b1d15068c11f9bcfbc0fc
한상희 <70365377+SangHui48@users.noreply.github.com>
2024-04-15 16:16:57 +0900

    4/15

--- baekjoon/2480.py
+++ baekjoon/2480.py
@@ -2,11 +2,20 @@ from collections import Counter
 
 data = list(map(int, input().split()))
 data_count = Counter(data)
-x = list(data_count.values()).
-
-if x == 3:
-    print(10000 + (x * 1000))
-elif x == 2:
-    print(1000 + (x * 100))
-else:
-    print(data.max() * 100)
+answer = 0
+max_val = 0
+for k, v in data_count.most_common():
+    if v == 3:
+        answer = 10000 + (k * 1000)
+        break
+    elif v == 2:
+        answer = 1000 + (k * 100)
+        break
+    else:
+        if k > max_val:
+            max_val = k
+            
+if max_val:
+    answer = (max_val) * 100
+    
+print(answer)
