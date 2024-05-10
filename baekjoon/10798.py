e9154988d5cc19bc4afb287e746ccaa19d5aa1ef
한상희 <70365377+SangHui48@users.noreply.github.com>
2024-05-10 16:21:41 +0900

    5/10

--- baekjoon/10798.py
+++ baekjoon/10798.py
@@ -1,3 +1,8 @@
 import sys
 
-a = []
+a = [ list(sys.stdin.readline().rstrip()) for _ in range(5) ]
+
+for j in range(15):
+    for i in range(5):
+        if j < len(a[i]):
+            print(a[i][j], end='')
