9bdeadbd927ccf98254e58b56a24402c5f7fab49
한상희 <70365377+SangHui48@users.noreply.github.com>
2024-08-13 17:42:32 +0900

    8/13 문풀중

--- baekjoon_java/baekjoon/src/b1018/Main.java
+++ baekjoon_java/baekjoon/src/b1018/Main.java
@@ -19,5 +19,34 @@ public class Main {
             board[i] = br.readLine();
         }
         br.close();
+
+        int result = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N - 7; i++) {
+            for (int j = 0; j < M - 7; j++) {
+                for (int k = 0; k < 2; k++) {
+                    char start = k == 0 ? 'W' : 'B';
+
+                    int count = 0;
+                    for (int n = i; n < i + 8; n++) {
+                        for (int m = j; m < j + 8; m++) {
+                            if (start != board[n].charAt(m)) {
+                                count++;
+                            }
+
+                            start = start == 'W' ? 'B' : 'W';
+                        }
+                        start = start == 'W' ? 'B' : 'W';
+                    }
+                    result = Math.min(result, count);
+
+                    if (result == 0) {
+                        System.out.println(0);
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(result);
     }
 }
