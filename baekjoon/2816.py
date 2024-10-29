21592399dd02a8fba5ba20a0ce6a730eb5e49aba
한상희 <70365377+SangHui48@users.noreply.github.com>
2024-10-29 18:00:41 +0900

    10/29

--- baekjoon/2816.py
+++ baekjoon/2816.py
@@ -2,7 +2,28 @@ import sys
 
 m_input = sys.stdin.readline
 n = int(m_input())
-arr = []
+chn = []
 for _ in range(n):
-    arr.append(m_input())
+    chn.append(m_input().rstrip())
 
+ans = []
+cur = 0
+
+while chn[cur] != 'KBS1':
+    cur += 1
+    ans.append(1)
+
+for _ in range(cur):
+    ans.append(4)
+
+cur = 0
+chn.remove('KBS1')
+chn = ['KBS1'] + chn
+while chn[cur] != 'KBS2':
+    cur += 1
+    ans.append(1)
+
+for _ in range(cur - 1):
+    ans.append(4)
+
+print(''.join(map(str, ans)))
